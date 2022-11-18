fb81ff74d1ef3aa893c24897184c58d0c76db592
Dana Lobell <danalobell@gmail.com>
2022-11-18 14:11:25 -0500

    client

---
+++ client/src/App.js
@@ -0,0 +1,24 @@
+import React, {useState, useEffect} from 'react';
+import {Route, Routes} from "react-router-dom";
+import './App.css';
+import Navbar from './Components/Navbar'
+import HomeContainer from './Components/Home/HomeContainer';
+import ArtMediaContainer from './Components/ArtMedia/ArtMediaContainer';
+import VictimsContainer from './Components/Victims/VictimsContainer';
+
+function App() {
+  
+
+  return (
+    <div className="App">
+      <Navbar />
+     <Routes>
+      <Route path = '/home' element = {<HomeContainer />} />
+      <Route path = '/media' element = {<ArtMediaContainer />} />
+      <Route path = '/victims' element = {<VictimsContainer />} />
+     </Routes>
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/Components/Victims/VictimsContainer.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function VictimsContainer() {
+  return (
+    <div>VictimsContainer</div>
+  )
+}
