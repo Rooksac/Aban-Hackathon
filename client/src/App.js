57c340833515531f49469bda907711167f658d64
Dana Lobell <danalobell@gmail.com>
2022-11-18 16:17:33 -0500

    toggling

--- client/src/App.js
+++ client/src/App.js
@@ -8,12 +8,30 @@ import ArtMediaContainer from './Components/ArtMedia/ArtMediaContainer';
 import VictimsContainer from './Components/Victims/VictimsContainer';
 
 function App() {
+  const [english, setEnglish] = useState(true)
+  const [data, setData] = useState([])
   
+  function handleLanguageChange(){
+    setEnglish(prev => !prev);
+  }
 
+  let language = '/englishes';
+
+  if(english){
+    language = '/englishes'
+  } else {
+    language = '/farsis'
+  }
+  
+  useEffect(() => {
+    fetch(`${language}`)
+  .then((res) => res.json())
+  .then(data => setData(data))}, [english])
+  console.log(data)
   return (
     <div className="App">
       <React.Fragment>
-        <ToggleSwitch label="Language" />
+        <ToggleSwitch label="Language" handleLanguageChange={handleLanguageChange}/>
       </React.Fragment>
       <Navbar />
      <Routes>
