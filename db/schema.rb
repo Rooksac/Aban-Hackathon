a99e94c9ebfaa82f58b48d92933821427041e4b5
root <root@DESKTOP-NH2VI3D.localdomain>
2022-11-18 13:46:09 -0500

    migration update

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_18_174806) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_18_175938) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -32,10 +32,10 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_18_174806) do
     t.string "date"
     t.string "location"
     t.string "cause"
-    t.string "image_small"
-    t.string "image_large"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image_small"
+    t.string "image_large"
   end
 
 end
