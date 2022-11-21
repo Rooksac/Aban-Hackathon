df4ac9276a257254fe097623b9683e57b98acfaf
root <root@DESKTOP-NH2VI3D.localdomain>
2022-11-21 05:20:37 -0500

    first deploy attempt

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 Rails.application.routes.draw do
-  resources :admins
-  resources :farsis
-  resources :englishes
-  
-  post '/login', to: 'admins#login'
-  get '/me', to: 'admins#me'
-  get '/previewenglish', to: 'englishes#preview'
-  get '/previewfarsi', to: 'farsis#preview'
+  namespace :api do
+    resources :admins
+    resources :farsis
+    resources :englishes
+    
+    post '/login', to: 'admins#login'
+    get '/me', to: 'admins#me'
+    get '/previewenglish', to: 'englishes#preview'
+    get '/previewfarsi', to: 'farsis#preview'
+  end
+
+  get "*path", to: "fallback#index", constraint
+  s: ->(req) { !req.xhr? && req.format.html? } 
 end
