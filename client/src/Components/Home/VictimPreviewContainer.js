7e65fcdcba82d132dbaf973228a7e535504c746f
root <root@DESKTOP-NH2VI3D.localdomain>
2022-11-20 19:24:21 -0500

    added filters and minor housekeeping

--- client/src/Components/Home/VictimPreviewContainer.js
+++ client/src/Components/Home/VictimPreviewContainer.js
@@ -1,6 +1,6 @@
 import React, {useState, useEffect} from 'react'
 import {Link} from 'react-router-dom';
-import VictimsPreviewCards from './VictimsPreviewCard'
+import VictimsPreviewCard from './VictimsPreviewCard'
 
 export default function VictimPreviewContainer({english}) {
   const [previewData, setPreviewData] = useState([])
@@ -20,7 +20,7 @@ export default function VictimPreviewContainer({english}) {
   return (
     <div className='victims-preview-container'>
       <div className = 'victim-preview-cards'>
-        {previewData.map(victim=><VictimsPreviewCards key = {victim.id} victim = {victim}/>)}
+        {previewData.map(victim=><VictimsPreviewCard key = {victim.id} victim = {victim}/>)}
         <Link to="/victims" className='victims-link'><h4>{english ? "Learn more about victims" : "درباره قربانیان بیشتر بدانید"}</h4></Link>
       </div>
     </div>
